4102d4212de2dddc763f9d62ee9926627431688f
tampopo-dandelion <goukaku2@icloud.com>
2022-01-28 17:44:50 +0900

    Bountyhunterのキルに失敗するとキルクールが倍々になるのを修正

--- Patches/PlayerContorolPatch.cs
+++ Patches/PlayerContorolPatch.cs
@@ -87,13 +87,15 @@ namespace TownOfHost
                     __instance.RpcMurderPlayer(target);
                     main.BitPlayers.Add(target.PlayerId, (__instance.PlayerId, 0f));
                     main.BeforeFixCooldown = opt.KillCooldown;
-                    opt.KillCooldown = main.BeforeFixCooldown / 4;
+                    opt.KillCooldown = main.BeforeFixCooldown / 12;
+                    PlayerControl.GameOptions.KillCooldown = main.BeforeFixCooldown;
                 }
                 else if (main.isBountyhunter(__instance) && target != player)
                 {
                 __instance.RpcMurderPlayer(target);
                 main.BeforeFixCooldown = opt.KillCooldown;
-                opt.KillCooldown = main.BeforeFixCooldown * 2;
+                opt.KillCooldown = main.BeforeFixCooldown * 4;
+                PlayerControl.GameOptions.KillCooldown = main.BeforeFixCooldown;
                 }
                 return false;
             }
